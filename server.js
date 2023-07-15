b2852113b85941e65680d5331f7b74a7dc25d320
Duck Rindle <duckrindle@protonmail.com>
2023-07-16 00:17:47 +0200

    upload rate computation mechanism

--- server.js
+++ server.js
@@ -3,21 +3,32 @@ const app = express();
 
 app.use(express.static('public'));
 
+var rate = 0;
+var bytes_count = 0;
+
+function compute_rate() {
+    rate = bytes_count / 1;
+    bytes_count = 0;
+    console.log("rate", rate);
+}
+setInterval(compute_rate, 1000);
+
 app.post("/", (req, res) => {
     //console.log("POST received");
 
     var text;
     req.on('data', function(chunk) {
         //console.log("data received:");
-        text = chunk.toString();
-        console.log("received", text);
+        //text = chunk.toString();
+        bytes_count += chunk.byteLength;
+        //console.log("received", bytes_count);
     });
 
     req.on('end', function() {
         //console.log("end");
+        var text = rate.toString();
         res.send(text);
-        //console.log("data sent:");
-        console.log("sent", text);
+        //console.log("sent", text);
     });
 
     req.on('error', (e) => {
